ee97d85da33b83290da5fce7d8141104362c0214
FSDonnelly <donnelly_frank@yahoo.com>
2019-05-14 22:42:59 -0700

    started logic for Lottery component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Ball from './Ball'
+import Lottery from './Lottery';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Ball num={17} />
-      <Ball num={7} />
-      <Ball num={28} />
-      <Ball num={7} />
+      <Lottery />
     </div>
   );
 }
